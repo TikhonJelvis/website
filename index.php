689d7d3acdf98609d557bdfd816a44fe31febd50
Tikhon Jelvis <tikhon@berkeley.edu>
2011-04-19 00:03:58 -0700

    Rewrote large amounts of text on the home page; added a news blurb to the top; added my picture to the page.

--- index.php
+++ index.php
@@ -16,10 +16,34 @@ include 'header.php'
 ?>
 
 <!-- This is the div where the actual contents of the page will go. -->
+<div class="content" id="news">
+<h1 class="header">
+News
+</h1>
+<p>
+I was on the team that got second place in this semester's CSUA hackathon (team
+blarg)! The project we made can be found
+on <a href="http://code.google.com/p/blarg">Google code</a>; you can read about
+the 
+event <a href="http://www.huffingtonpost.com/marissa-louie/for-the-win-at-the-berkel_b_844749.html"
+title="An article about the hackathon.">here</a>. Apart from glory, we also all
+got Amazon Kindles!
+</div>
 <div class="content" id="main">
 <h1 class="header">
 Welcome
 </h1>
+<div class="caption">
+  <a href="img/me_at_casa_mila_med.jpg" title="Click for full size">
+    <img src="img/me_at_casa_mila_med.jpg" alt="A nice picture of me in Spain"
+/> 
+  </a>
+  <p>
+    A gratuitous picture of me on the roof of Casa Mil&agrave; in
+    Barcelona. You can barely make out the Sagrada Fam&iacute;lia in the
+    background. 
+ </p>
+</div>
 <p>
 I am a freshman Electrical Engineering and Computer Sciences (EECS)
 major at the University of California, Berkeley. I am particularly
@@ -49,7 +73,7 @@ and I will probably get a more thorough grounding in it than I would
 in a pure CS major.
 </p>
 <h2 class="header">
-Projects
+Programming
 </h2>
 <p>
 I have been programming on my own for a relatively long time now. My
@@ -58,7 +82,7 @@ towards the latter half of 2004, when I started learning
 JavaScript. The state of JavaScript at that time was very different
 from now; I remember commenting out the actual code inside the
 <span class="code">script</span> tag because older browsers did not
-acknowledge script tags at all. This has not been necessary for years!
+acknowledge script tags at all&mdash;a positively ancient practice now!
 </p>
 <p>
 Ever since learning JavaScript on my own, I have continued to
@@ -67,16 +91,12 @@ mechanics&mdash;I was having enough difficulty doing anything at all,
 so I didn't even consider trying to finish something coherent. Later
 on, I began to work on increasingly large projects; however, thanks to
 my poor practices and lack of back ups, most of my very early projects
-are irrevocably gone.
+have disappeared irretrievably.
 </p>
 <p>
-After this, I continued to program in my spare time, sometimes using
-my new skills for school projects, but generally only playing
-around. During freshman year of high school, I decided to learn a new
-language and picked Perl; while I did not end up learning it in any
-great depth, it is still I tool I use once in a while for various
-things.
-</p>
+As time went on, my technical grasp of programming improved, as did my
+practices. This means that I have a couple relatively large, coherent and
+somewhat well-written projects from the latter half of high school.
 <h3 class="header">
   Simulation
 </h3>
@@ -90,26 +110,41 @@ project for physics, so decided, along with some friends, to write a
 program for it.
 </p>
 <p>
-The chess game and the simulation were not written alone; I worked with
-Jacob Taylor, a friend who is now studying CS at Stanford. His page can be found
-<a href="http://www.stanford.edu/~jacobt">here</a>. For the simulation, he
-worked on modeling the polygons, doing all of the actual math while I
-worked on the interface and putting the entire project together. For
-the chess game, he ported a chess AI he had written earlier to
-JavaScript. While we both have a very similar JavaScript plotting tool
-on our sites, we actually came up with them completely independently.
+We ended up making a fairly nice 2D polygon collision simulation with a
+thorough UI that, while weak on user experience and not very intuitive, is
+relatively useable and rather capable.
+</p>
+<p>
+This project, even now, is the my largest by lines of code&mdash;it has
+somewhere around 8500 lines written by two people. Of course, one of the main
+reasons for this is Java: not exactly the most concise language. That said,
+this was a fairly complicated project; <a href="simulation" title="User
+Guide">here</a> is a user guide for it. It can be found on
+<a href="http://www.github.com/TikhonJelvis/simulation">github</a> as well.
+</p>
+<h2 class="header">
+Other Projects
+</h2>
+<p>
+I have finished several projects since the collision simulation. Right now,
+some projects can be accessed via the menu above: <a href="chess" title="Maptac
+Chess"> chess </a> and <a href="draw" title="Draw with JavaScript!">
+drawing </a>. However, this is nowhere near an exhaustive list: I mean to put
+up a proper project page, but have yet to do so. 
+</p>
+<p>
+All my projects from now on should be up
+on <a href="http://www.github.com/TikhonJelvis" title="My github
+page.">github</a>. I also mean to move my old projects to github (they are
+mostly on Google code right now), but I will probably never get around to it. 
 </p>
 <h2 class="header">
 This Site
 </h2>
 <p>
 This site is currently hosted on the EECS instructional Unix server at
-inst.eecs.berkeley.edu. This page is build with xhtml and
-CSS. The typeface used is Verdana, which is my favorite web typeface,
-by far. Most of the text on this page is just padding to make it look
-better; I will add text worth reading in the future. Perhaps I will
-also add some pictures, or something.
-</p>
+inst.eecs.berkeley.edu. Both the xhtml and css should be standards compliant;
+feel free to complain to me if it isn't! 
 </div>
 
 <?php
