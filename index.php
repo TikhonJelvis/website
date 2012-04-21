66723cbb3a78d0edb1a22ba93ac6302989e9e535
Tikhon  Jelvis <tikhon.jelvis@gmail.com>
2012-04-21 02:22:50 -0700

    Added a summary of programming languages I've used. Yay.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $homePath = "."; 
 include "head.php";
 ?>
+<link rel="stylesheet" type="text/css" href="css/index.css" />
 
 <title>
   Tikhon Jelvis
@@ -29,6 +30,67 @@ event <a href="http://www.huffingtonpost.com/marissa-louie/for-the-win-at-the-be
 title="An article about the hackathon.">here</a>. Apart from glory, we also all
 got Amazon Kindles!
 </p>
+</div>
+<div class="content" id="languages">
+<h1 class="header">
+Programming Languages
+</h1>
+<p>
+  Here are the programming languages I'm familiar with, roughly in
+  order of preference:
+</p>
+<ul class="languages">
+  <li class="haskell">
+    <a href="http://en.wikipedia.org/wiki/Haskell_(programming_language)">
+      <img src="img/haskell.png" alt="Haskell" title="Haskell" />
+    </a>
+  </li>
+  <li class="javascript">
+    <a href="http://en.wikipedia.org/wiki/JavaScript">
+      <img src="img/javascript.png" alt="JavaScript" title="JavaScript" />
+    </a>
+  </li>
+  <li class="elisp">
+    <a href="http://en.wikipedia.org/wiki/Emacs_Lisp">
+      <img src="img/elisp.png" alt="Emacs Lisp" title="Emacs Lisp" />
+    </a>
+  </li>
+  <li class="scheme">
+    <a href="http://en.wikipedia.org/wiki/Scheme_(programming_language)">
+      <img src="img/scheme.png" alt="Scheme" title="Scheme" />
+    </a>
+  </li>
+  <li class="c">
+    <a href="http://en.wikipedia.org/wiki/C_(programming_language)">
+      <img src="img/c.png" alt="C" title="C" />
+    </a>
+  </li>
+  <li class="prolog">
+    <a href="http://en.wikipedia.org/wiki/Prolog_(programming_language)">
+      <img src="img/prolog.png" alt="Prolog" title="Prolog" />
+    </a>
+  </li>
+  <li class="tex">
+    <a href="http://en.wikipedia.org/wiki/TeX">
+      <img src="img/tex.png" alt="TeX" title="TeX" />
+    </a>
+  </li>
+  <li class="java">
+    <a href="http://en.wikipedia.org/wiki/Java_(programming_language)">
+       <img src="img/java.png" alt="Java" title="Java" />
+    </a>
+  </li>
+  <li class="python">
+    <a href="http://en.wikipedia.org/wiki/Python_(programming_language)">
+      <img src="img/python.png" alt="Python" title="Python" />
+    </a>
+  </li>
+  <li class="perl">
+    <a href="http://en.wikipedia.org/wiki/Perl">
+      <img src="img/perl.png" alt="Perl" title="Perl" />
+    </a>
+  </li>
+</ul>
 </div>
 <div class="content" id="main">
 <h1 class="header">
