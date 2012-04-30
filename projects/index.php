3f062bb8f795df17d0b83680255eb76137b28b69
Tikhon  Jelvis <tikhon.jelvis@gmail.com>
2012-04-30 01:36:58 -0700

    Started working on my projects page. However, I am not very happy with how it looks right now; I think I'll make its design more elaborate in the future.

---
+++ projects/index.php
@@ -0,0 +1,59 @@
+<?php $homePath = '..'; include '../head.php'; ?>
+<title>
+  Projects
+</title>
+
+</head>
+<body>
+  <?php include '../header.php'; ?>
+
+  <div class="content">
+    <h1 class="header">
+      Projects
+    </h1>
+
+    <p>
+      I have worked on a bunch of side projects in my spare time. The
+      projects vary drastically, but have one thing in common: they
+      were all fun to work on. 
+    </p>
+    <p>
+      Some of the projects were solo efforts, but many of the others
+      would not have been possible without help from some eminently
+      competent friends. Overall, these were the more fun
+      projects. I've linked to the websites of my collaborators for
+      every project; they are all worth visiting.
+    </p>
+  </div>
+
+  <div class="content">
+    <h1 class="header">
+      High School
+    </h1>
+    <p>
+      While I did a bunch of programming before high school, I never
+      made much worth keeping. Additionally, I didn't use source
+      control or have backups; predictably, I do not have most of my
+      really early work. Luckily it's only interesting as a window
+      into my personal development, so it's not a significant
+      loss. And I learned a valuable lesson: I now let the rest of the
+      world back my work up for me.
+    </p>
+
+    <h2 class="header">
+      <a href="../chess">Chess</a>
+    </h2>
+
+    <p>
+      My last project in high school was a web-based chess game. I had
+      initially started it in my sophomore year in an effort to learn
+      prototype.js, but wrote myself into a corner. I rewrote it from
+      the ground up my senior year (still using prototype),
+      and <a href="http://www.stanford.edu/~jacobt">Jacob Taylor</a>
+      ported his LISP chess AI to JavaScript for it.
+    </p>
+  </div>
+
+  <?php include '../footer.php' ?>
+</body>
+</html>
