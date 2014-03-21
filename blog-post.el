50a86aca6f44631593db51e27e93ebe5a4552bf2
Tikhon Jelvis <tikhon@jelv.is>
2014-03-21 02:27:48 -0700

    Added an elisp function to publish my drafts.

--- blog-post.el
+++ blog-post.el
@@ -1,7 +1,7 @@
 ;;; Handy functions for automatically creating drafts and publishing
 ;;; blog posts using my Hakyll setup.
 
-(defun draft-post (name)
+(defun draft-post (name &optional publish)
   "Create a new draft blog post with the given name. If the post
 already exists, open it up instead.
 
@@ -9,7 +9,13 @@ This creates the necessary directory structure and prepopulates a
 generated index.md with a title and author field. All the actual
 file munging is done by a simple Haskell script."
   (interactive "sName: ")
-  (message (format "Running `blog-post '%s'" name))
-  (shell-command (format "blog-post.hs '%s'" name))
+  (let ((flag (if publish "-p" "")))
+    (message (format "Running `blog-post '%s' %s" name flag))
+    (shell-command (format "blog-post.hs '%s' %s" name flag)))
   (find-file (format "~/Public/website/drafts/%s/index.md" name))
   (end-of-buffer))
+
+(defun publish-post (name)
+  "Publishes the post with the given name."
+  (interactive "sName: ")
+  (draft-post name t))
