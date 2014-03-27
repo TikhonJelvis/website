0140337e065786029651c197c0402121d4109160
Tikhon Jelvis <tikhon@jelv.is>
2014-03-27 13:16:23 -0700

    Simplified some of the code to make it more didactic.

--- drafts/Generating Mazes with Inductive Graphs/misc/Maze.hs
+++ drafts/Generating Mazes with Inductive Graphs/misc/Maze.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE ParallelListComp    #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE ViewPatterns        #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ParallelListComp           #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE ViewPatterns               #-}
 module Maze where
 
 import           Control.Applicative               ((<$>), (<*>))
@@ -10,45 +12,50 @@ import           Control.Monad.Random              (MonadRandom(..), RandomGen, 
 import           Control.Monad.State
 
 import qualified Data.Graph.Inductive              as Graph
-import           Data.Graph.Inductive              (DynGraph, Graph, match)
+import           Data.Graph.Inductive              (DynGraph, Graph, match, (&))
 import           Data.Graph.Inductive.PatriciaTree (Gr)
 import qualified Data.List                         as List
 
 import           Debug.Trace                       (trace, traceShow)
 
-type Maze = Gr Pos Direction
+data Shape = Square deriving (Show, Eq)
+
+type Maze = Gr Shape (Direction, Bool)
 
 type Pos = (Int, Int)
 
-data Direction = Horizontal | Vertical deriving (Show, Eq)
+newtype Direction = Direction Rational deriving (Eq, Num)
 
--- | A cell and its edges: first outgoing then incoming.
-type Cell = (Pos, [Direction])
+instance Show Direction where show (Direction d) = show d
 
--- | Extract the walls from a maze graph.
-cells :: Maze -> [Cell]
-cells maze = toWalls <$> Graph.nodes maze
-  where toWalls node =
-          let walls = dir <$> Graph.out maze node in
-           case Graph.lab maze node of
-            Just cell -> (cell, walls)
-            Nothing   -> error "Malformed graph!"
-        dir (_, _, d) = d
+data Wall = Wall Direction Bool deriving (Show, Eq)
 
 -- | Generate a graph representing an n × m grid. The nodes are
 -- labelled with their (x, y) position.
 grid :: Int -> Int -> Maze
 grid width height = Graph.mkGraph nodes edges
-  where nodes = zip [0..] [(x, y) | (y, x) <- map (`divMod` width) [0..width * height - 1]]
-        edges = [(n, n', Vertical) | (n,_) <- nodes, (n',_) <- nodes, n - n' == 1 && n `mod` width /= 0]
-             ++ [(n, n', Horizontal) | (n,_) <- nodes, (n',_) <- nodes, n - n' == width]
+  where nodes = zip [0..height * width - 1] $ repeat Square
+        edges = [(n, n', (180, True)) | (n,_) <- nodes, (n',_) <- nodes, n - n' == 1 && n `mod` width /= 0]
+             ++ [(n, n', (90, True)) | (n,_) <- nodes, (n',_) <- nodes, n - n' == width]
 
 -- | Generate a depth-first traversal of the given graph.
-dfs :: Graph g => [Graph.Node] -> g a b -> [Graph.Node]
 dfs [] _                           = []
 dfs _ g | Graph.isEmpty g          = []
 dfs (v:vs) (match v -> (Just c, g)) = v : dfs (Graph.suc' c ++ vs) g
 dfs (_:vs) g                       = dfs vs g
+
+edfs [] _                                   = []
+edfs _ g | Graph.isEmpty g                  = []
+edfs ((v, p) : vs) (match v -> (Just c, g)) =
+  (v, p) : edfs (map (v,) (Graph.suc' c) ++ vs) g
+edfs (_:vs) g                               = edfs vs g
+
+edfsR [] _                                   = return []
+edfsR _ g | Graph.isEmpty g                  = return []
+edfsR ((v, p) : vs) (match v -> (Just c, g)) =
+  do next <- shuffle $ (v,) <$> Graph.suc' c
+     ((v, p) :) <$> edfsR (next ++ vs) g
+edfsR (_:vs) g                                = edfsR vs g
 
 -- | Generate a maze by traversing a graph and deleting any edges we
 --   follow. The remaining edges then form the "walls" of the maze.
@@ -59,9 +66,12 @@ maze width height = execStateT (go [(0, 0)] cells) cells
         go ls g | Graph.isEmpty g         = return ()
         go ((v, parent):vs) (match v -> (Just c, g)) = do
           next <- shuffle $ zip (Graph.neighbors' c) (repeat v)
-          modify $ Graph.delEdges [(v, parent), (parent, v)]
+          modify $ toggleEdge v parent
           go (next ++ vs) g
         go (_:vs) g = go vs g
+        toggleEdge n p (match n -> (Just (inn, _, l, out), g)) =
+          (map (toggle p) inn, n, l, map (toggle p) out) & g
+        toggle p ((dir, wall), p') = if p == p' then ((dir, not wall), p') else ((dir, wall), p')
 
 -- | Choose a random element out of a list.
 choose [] = error "Cannot choose from empty list!"
