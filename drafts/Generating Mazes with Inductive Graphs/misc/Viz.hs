0b3be589ff951e54a858193675aa481f298e613c
Tikhon Jelvis <tikhon@jelv.is>
2014-04-06 01:42:19 -0700

    Updated visualization code to support the animations I want to write soon.

--- drafts/Generating Mazes with Inductive Graphs/misc/Viz.hs
+++ drafts/Generating Mazes with Inductive Graphs/misc/Viz.hs
@@ -17,24 +17,50 @@ blue = "#0b4faa"
 
 lightBlue = "#90A9CB"
 
-graphViz ctx@(_, node, _, _) graph = [str|
+bothN :: Graph.Node -> Gr a b -> Gr a b -> Bool
+bothN node g₁ g₂ = node `elem` Graph.nodes g₁ && node `elem` Graph.nodes g₂
+
+bothE :: Graph.Edge -> Gr a b -> Gr a b -> Bool
+bothE edge g₁ g₂ = edge `elem` Graph.edges g₁ && edge `elem` Graph.edges g₂
+
+nodeStyle :: Graph.Node -> Graph.Node -> Gr a b -> Gr a b -> String
+nodeStyle n node full rest
+  | n == node          = [str|[color="$blue$" style="filled" fillcolor="$lightBlue$"]|]
+  | bothN n full rest = ""
+  | otherwise         = [str|[color="white" style="filled" fillcolor="white" fontcolor="white"]|]
+
+edgeStyle :: Graph.Edge -> Graph.Node -> Gr a b -> Gr a b -> String
+edgeStyle (a, b) node full rest
+  | a == node || b == node    = [str|[color="$blue$"]|]
+  | bothE (a, b) full rest = ""
+  | otherwise              = [str|[color="white"]|]
+
+decomposition full (_, node, _, _) rest = [str|
 digraph fgl {
 	margin = "0"
 	page = "4"
 	size = "4"
 	ratio = "fill"
-        $:node$ [color="$blue$" style="filled" fillcolor="$lightBlue$"]
-	#node in Graph.nodes graph:$:node$|
+	#n in Graph.nodes full:$:n$ $nodeStyle n node full rest$|
         # 
-        #node' in Graph.suc' ctx:$:node$ -> $:node'$ [color="$blue$"]|
-        # 
-        #node' in Graph.pre' ctx:$:node'$ -> $:node$ [color="$blue$"]|
-        #
-        #(a, b) in Graph.edges graph:$:a$ -> $:b$|
+        #(a, b) in Graph.edges full:$:a$ -> $:b$ $edgeStyle (a, b) node full rest$|
         #
 }
 |]
 
-anyViz (matchAny -> (ctx, graph)) = graphViz ctx graph
+anyViz full (matchAny -> (ctx, graph)) = decomposition full ctx graph
 
-nViz n (match n -> (Just ctx, graph)) = graphViz ctx graph
+nViz full n (match n -> (Just ctx, graph)) = decomposition full ctx graph
+
+normal graph = [str|
+digraph fgl {
+	margin = "0"
+	page = "4"
+	size = "4"
+	ratio = "fill"
+	#node in Graph.nodes graph:$:node$|
+        # 
+        #(a, b, l) in Graph.labEdges graph:$:a$ -> $:b$ [label="$:l$"]|
+        #
+}                
+|]
