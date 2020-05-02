3912bd12d7ad10482d085dc930eb825f46d20f5f
Tikhon Jelvis <tikhon.jelvis@target.com>
2020-05-02 09:52:51 -0700

    Set LOCALE_ARCHIVE in shell.nix.

--- shell.nix
+++ shell.nix
@@ -8,4 +8,11 @@ let
 in
 pkgs.lib.overrideDerivation website.env (old: {
   nativeBuildInputs = old.nativeBuildInputs ++ haskellDevelopmentTools;
+
+  # Nix's glibc has weird locale settings on Linux, which make Haskell
+  # binaries unable to handle UTF-8 by default. This fixes that
+  # problem.
+  LOCALE_ARCHIVE =
+    pkgs.lib.optionalString pkgs.buildPlatform.isLinux "${pkgs.buildPackages.glibcLocales}/lib/locale/locale-archive";
+  LANG = "C.UTF-8";
 })
